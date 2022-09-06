506139865cf54f2019c59137f8a1ed90d5ee0e04
vatsal278 <vatsalchaturvedi278@gmail.com>
2022-09-06 17:30:07 +0530

    POC for download file from server

--- main.go
+++ main.go
@@ -3,32 +3,40 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/vatsal278/go-redis-cache"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	cacher := redis.NewCacher(redis.Config{
-		Addr: "localhost:9096",
-	})
+	//cacher := redis.NewCacher(redis.Config{
+	//	Addr: "localhost:9096",
+	//})
 	r := mux.NewRouter()
-	r.HandleFunc("/download/{image}", func(w http.ResponseWriter, r *http.Request) {
-		v := mux.Vars(r)
-		b, err := cacher.Get(v["image"])
+	r.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+
+		v := r.URL.Query().Get("file")
+		b, err := ioutil.ReadFile("uploads/" + v)
+		//b, err := cacher.Get(v)
 		if err != nil {
 			log.Print(err.Error())
+			return
 		}
-		w.Header().Set("Content-Disposition", "attachment; filename=Observation.png")
+		w.Header().Set("Content-Disposition", "attachment; filename="+v)
 		w.Header().Set("Content-Type", "application/octet-stream")
-		w.Header().Set("Content-Length", fmt.Sprint(len(b)))
+
 		if _, err := w.Write(b); err != nil {
 			log.Println("unable to write image.")
+			return
 		}
 	}).Methods("GET")
 	r.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("File Upload Endpoint Hit")
+		err := r.ParseMultipartForm(10240)
+		if err != nil {
+			log.Print(err.Error())
+			return
+		}
 		file, handler, err := r.FormFile("file")
 		if err != nil {
 			log.Println("Error Retrieving the File")
@@ -37,17 +45,17 @@ func main() {
 		}
 		defer file.Close()
 		log.Printf("Uploaded File: %+v\n", handler.Filename)
-		tempFile, err := ioutil.TempFile("", handler.Filename)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer tempFile.Close()
 		fileBytes, err := ioutil.ReadAll(file)
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
-		tempFile.Write(fileBytes)
-		cacher.Set(handler.Filename, tempFile, 0)
+		//cacher.Set(handler.Filename, fileBytes, 0)
+		err = ioutil.WriteFile("uploads/"+handler.Filename, fileBytes, 0644)
+		if err != nil {
+			log.Printf("failed writing to file: %s", err)
+			return
+		}
 		log.Println("Successfully Uploaded File\n")
 	}).Methods("POST")
 	http.Handle("/", r)
